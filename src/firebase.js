ecdcfbf8553b8ee0e6cc34f366331e685e4fe6a1
Sosiska0v0test <117019284+Sosiska0v0test@users.noreply.github.com>
2025-01-07 20:35:42 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,9 +3,9 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDwyRhCwLMxRBTcWFU4PJdu6IRIvwee2So",
-  authDomain: "firebase-authentication-1tvf.vercel.app",
+  authDomain: "fir-authentication-8fec3.firebaseapp.com",
   projectId: "fir-authentication-8fec3",
-  storageBucket: "fir-authentication-8fec3.appspot.com",
+  storageBucket: "fir-authentication-8fec3.firebasestorage.app",
   messagingSenderId: "613286786106",
   appId: "1:613286786106:web:f69a3a0bb76a7b918f3103",
   measurementId: "G-TK8LK3VT87",
